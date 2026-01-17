ba8dd833db9297fde592a297fcb57332d3ad704d
Krijn Haasnoot <krijnhaasnoot@gmail.com>
2026-01-17 16:32:37 +0100

    Initial Commit

---
+++ petnames/petnamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  petnamesApp.swift
+//  petnames
+//
+//  Created by Krijn Haasnoot on 17/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct petnamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ petnamesTests/petnamesTests.swift
@@ -0,0 +1,17 @@
+//
+//  petnamesTests.swift
+//  petnamesTests
+//
+//  Created by Krijn Haasnoot on 17/01/2026.
+//
+
+import Testing
+@testable import petnames
+
+struct petnamesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
